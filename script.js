b97d73a8d930fc451684b1af3ba886ba3d01915a
Mxmac <pro.maximeandre@gmail.com>
2025-01-27 11:25:09 +0100

    test verif coté client

---
+++ script.js
@@ -0,0 +1,38 @@
+let form = document.querySelector('form');
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let firstName = document.getElementById("firstName");
+let description = document.getElementById("description");
+let errorMessages = document.querySelectorAll('.error');
+
+function validateField(input, message) {
+    let errorElement = input.nextElementSibling;
+    if (!input.value.trim()) {
+        errorElement.textContent = message;
+        errorElement.classList.add('active');
+        return false;
+    } else {
+        errorElement.textContent = "";
+        errorElement.classList.remove('active');
+        return true;
+    }
+}
+
+
+form.addEventListener('submit', function (event) {
+    let isValid = true;
+
+    if (!validateField(name, "Le champ Nom est requis.")) isValid = false;
+    if (!validateField(firstName, "Le champ Prénom est requis.")) isValid = false;
+    if (!validateField(email, "Le champ E-mail est requis.")) isValid = false;
+    if (!email.validity.valid) {
+        email.nextElementSibling.textContent = "L'adresse e-mail n'est pas valide.";
+        email.nextElementSibling.classList.add('active');
+        isValid = false;
+    }
+    if (!validateField(description, "Le champ Description est requis.")) isValid = false;
+
+    if (!isValid) {
+        event.preventDefault();
+    }
+}, false);
