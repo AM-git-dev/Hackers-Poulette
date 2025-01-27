ce551da9d5abfcbb9749bc6941d760ef707c9607
Mxmac <pro.maximeandre@gmail.com>
2025-01-27 14:21:42 +0100

    verification backend de remplissage des champs avant insertion dans bdd

--- Models/FormModel.php
+++ Models/FormModel.php
@@ -3,6 +3,12 @@ class FormModel
 {
     public function savedata($name, $firstname, $email, $file, $description)
     {
+        if (empty(trim($_POST['name'])) || empty(trim($_POST['firstname'])) || empty(trim($_POST['email'])) || empty(trim($_POST['description']))) {
+            die("Tous les champs sont requis.");
+
+        } else {
+
+        }
         try {
             $bdd = DbConnection::getBdd();
             $query = $bdd->prepare('INSERT INTO hackerspoulette (name, firstname, email, file, description) VALUES(?, ?, ?, ?, ?)');
